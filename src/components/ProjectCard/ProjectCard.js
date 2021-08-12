79afcda9dfc17f5996b13c46b5cbf14e877806b9
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-08-11 19:03:51 -0500

    added css variable

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,22 +3,25 @@ import './style.css'
 // Props are passed through our functional component.
 function ProjectCard(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+
+    <a className="portfolio-div-link" href={props.url} rel="noreferrer" target="_blank">
+      <div className="card">
+        <div className="img-container">
+          <img alt={props.name} src={props.image} />
+        </div>
+        <div className="content">
+          <ul>
+            <li>
+              <a href={props.url} rel="noreferrer" target="_blank">{props.name}</a><span><a href={props.github} rel="noreferrer" target="_blank"><i className="fa fa-github"></i></a></span>
+            </li>          
+            <li>
+              {props.tech}
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="content">
-        <ul>
-          <li>
-          <a href={props.url} rel="noreferrer" target="_blank">{props.name}</a><span><a href={props.github} rel="noreferrer" target="_blank"><i className="fa fa-github"></i></a></span>
-          </li>
-          
-          <li>
-          {props.tech}
-          </li>
-        </ul>
-      </div>
-    </div>
+    </a>
+    
   );
 }
 
