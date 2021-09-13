db28853c29b2bff5730d9648ea1c113e40824b16
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-09-13 15:01:31 -0500

    picture and css edits

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,25 +1,22 @@
 
-import ProfilePic from '../../assets/img/swiss.JPG'
+import ProfilePic from '../../assets/img/cwint4.JPG'
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import Image from "react-bootstrap/Image"
 import './style.css'
 
 function AboutMe() {
     return (
         <Container id="about-me-container" fluid>
-            <Row>
+            <div>
                 <h1 id="about-me-title">About Me</h1> 
-            </Row>
-            <Row id="about-me-wrapper">
-                <Col id="profile-pic" sm={3}>
-                    <Image src={ProfilePic} alt="profile-pic"/>
-                </Col>
-                <Col sm={9} id="text-div">
+            </div>
+            <div id="about-me-wrapper">
+                <div id="profile-pic" >
+                    <img src={ProfilePic} alt="profile-pic"/>
+                </div>
+                <div  id="text-div">
                     <p id="about-me-text">Hi and welcome to my website made from scratch using React! I'm a full-stack software developer, blockchain enthusiast, and musician here in Austin, TX. I love creating and enhancing peoples experience with an attention to detail beyond thier expectations. I'm about to graduate from the Univerity of Texas with a certificate in Full-Stack Development focused on the MERN stack. Over the years, I've built an unique background in technology, music, leadership, and sales that allows me to overcome any challenge. < br/>With the skills I've built over the years necessary to excel in the Silicon Hills, I'm now turning my focus towards my passion for tech and ready to create!</p>
-                </Col>                
-            </Row>                                 
+                </div>                
+            </div>                                 
         </ Container>
     )
 }
