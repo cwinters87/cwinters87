7aca09f33eb6329549b6580afa28092cdb680461
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-09-15 12:46:32 -0500

    about me edit

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ function AboutMe() {
                     <img src={ProfilePic} alt="profile-pic"/>
                 </div>
                 <div  id="text-div">
-                    <p id="about-me-text">I'm a full-stack software developer, blockchain enthusiast, and musician in Austin, TX. I love creating and enhancing peoples experience with attention to detail beyond thier expectations. I graduated from the Univerity of Texas with a certificate in Full-Stack Development focused on the MERN Stack in August of 2021. Over the years, I've built a unique background in technology, music, leadership, and sales that allows me to overcome any challenge. < br/>With the skills I've built over the years necessary to excel in the Silicon Hills, I've turned my focus towards my passion for tech and now get to create cool websites and applications.</p>
+                    <p id="about-me-text">I'm a full-stack software developer, blockchain enthusiast, and musician in Austin, TX. I love creating and enhancing peoples experience with attention to detail beyond thier expectations. I graduated from the Univerity of Texas with a certificate in Full-Stack Development focused on the MERN Stack in August of 2021. Over the years, I've built a unique background in technology, music, leadership, and sales that allows me to overcome any challenge. < br/>With the skills I've built over the years necessary to excel in the Silicon Hills, I've turned my focus towards my passion for tech and now get to create websites and applications.</p>
                 </div>                
             </div>                                 
         </ Container>
